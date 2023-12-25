9d25607f2bc3005a71992d0b758a1256ac393919
nao mac <sagalumaata@gmail.com>
2023-12-26 01:54:45 +0900

    content_script修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,15 @@ const manifest = defineManifest({
 	manifest_version: 3,
 	name: "web_memo",
 	version: "1.0.0",
-	// content_scripts:[
-	// 	{
-	// 		matches: ['http://*/*', 'https://*/*', 'file:///*'],
-	// 		js:["src/contentscript/script.ts"],
-	// 	},
-	// ],
-	// "content_scripts": [
-	// 	{
-	// 		"js": ["src/content.jsx"],
-	// 		// "js": ["src/contentScript/script.tsx"],
-	// 		"matches": ["https://www.google.com/*"]
-	// 	}
-	// ],
+	content_scripts:[
+		{
+			matches: ['http://*/*', 'https://*/*', 'file:///*'],
+			js:["src/content_scripts.js"],
+		},
+	],
 	action: {
 		default_popup: "src/popup/popup.html",
 	},
-	"content_scripts": [
-		{
-			"matches": [
-				"http://*/*",
-				"https://*/*"
-			],
-			"js": [ "src/content_scripts.js" ]
-		}
-	]
 });
 
 // https://vitejs.dev/config/
